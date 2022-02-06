48e2802ee1fd20ff404fbfc1827b2a2f0dd9a1a0
chenagoold <chenagoold@gmail.com>
2022-02-06 22:47:48 +0300

    06.02.21

--- webformself/lessons-3/index.js
+++ webformself/lessons-3/index.js
@@ -1,13 +1,37 @@
 const express = require('express')
+const lm = require('lmclient')
 const app = express()
 const bodyParser = require('body-parser')
-const weatherRequest = require('./requests/weather.requests.js') 
+const weatherRequest = require('./requests/weather.requests.js')
+var client = new lm.LMClient({
+        host:       '127.0.0.1',                // адрес сервера
+        port:       3000,                       // порт сервера
+        login:      'login1',                   // логин
+        password:   'password',                 // пароль
+        reconnect:  true,                       // автоматически переподключаться при ошибках и разрывах связи
+        opros:      false,                      // тип учетной записи "опрос"
+        client:     true                        // тип учетной записи "клиент"
+}); 
 
 //c789a8de97310445a9cd749db08dac2e
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
+
+client.on('connecting', function(host, port){
+    console.log('connecting to ' + host + ':' + port);
+});
+
+client.on('connect', function(){
+    console.log('connected');
+});
+client.connect();
+
+//app.get('/about',(req,res) =>{
+ //   const {host,port} =  
+
+//})
 
 
 
